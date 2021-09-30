453c3187a80817165c8bdb1e8f751ee6974f2a59
Guido Mintjes <guidomintjes@gmail.com>
2021-09-30 21:32:37 +0200

    Add project files.

---
+++ TCPChatClient/TCPChatClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCPChatClient {
+
+    class TCPChatClient {
+
+        static void Main(string[] args) {
+
+            Console.Title("TCP Chat Client Demo");
+
+        }
+    }
+}
---
+++ TCPChatServer/TCPChatServer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCPChatServer {
+    class TCPChatServer {
+        static void Main(string[] args) {
+
+            Console.Title("TCP Chat Server Demo");
+            
+        }
+    }
+}
