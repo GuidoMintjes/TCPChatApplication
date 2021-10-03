f8b03819a3d911cdf262cb457789c71b3da568cc
Guido Mintjes <guidomintjes@gmail.com>
2021-10-03 12:49:34 +0200

    Updates!

--- TCPChatServer/ChatApp.cs
+++ TCPChatServer/ChatApp.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using TCPChatServer;
 
 namespace TCPChatServer {
     class ChatApp {
---
+++ TCPChatServer/Packet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace TCPChatServer {
+
+    // A packet is a piece of information that gets sent over a network
+    class Packet {
+
+
+
+    }
+}
