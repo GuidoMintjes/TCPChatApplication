272cc8ab9b647bd6268a6ce3193132a66410d8c0
Guido Mintjes <guidomintjes@gmail.com>
2021-10-10 21:48:17 +0200

    Figured out packet class and added

---
+++ TCPChatServer/TCPSend.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TCPChatServer {
+    class TCPSend {
+
+        // Send a nice welcoming message to a client that just connected
+        public static void WelcomeClient() {
+
+        }
+    }
+}
